6c7b9868955fec05bf0cb7ba677a2ac34ade932b
Siarhei <taler34@gmail.com>
2021-06-07 00:10:17 +0300

    EqualityCheck

---
+++ EqualityCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace EqualityCheck
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckEquality(6,6));
+        }
+
+        public static bool CheckEquality(object a, object b)
+        {
+          bool check= a.Equals(b);
+          return check;
+        }
+    }
+}
